850d865c4999f247d26941a1ecb9976b6ba88e53
Fahad Mukhtar <fahad.mukhtar@stud.uni-bamberg.de>
2023-03-29 00:13:15 +0200

    initial_commit

---
+++ PythonLearning/main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print('My name is fahad')
+
+    #variables
+    new_variable = 10
+    print(new_variable)
+
+    patient_name = 'Test Patient'
+    patient_age = 20
+    rating = 4.9
+    is_new = True
+
+#input
+    name = input('What is your name? ')
+    color = input('Which colour do you like? ')
+    print(name+' likes '+color)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
